313364966c16e2407cb1fc22508d4417214d1127
blanning <blanning@all-mode.com>
2019-03-15 11:18:20 -0400

    test: Update testing in preparation for multiple file read method testing

--- test/unit.js
+++ test/unit.js
@@ -9,34 +9,36 @@ const Log = require('./../models/log.js');
 describe('log', () => {
 
   it('should return a new log object', () => {
-    const l = Log.logfile('./test.log');
+    const l = Log.logfile('./test/fixtures/short-test.log');
 
     expect(l).to.be.an.object();
   });
 
   it('should have a path property that equals the path passed in', async () => {
-    const l = await Log.logfile('./test.log');
+    const l = await Log.logfile('./test/fixtures/short-test.log');
 
-    expect(l).to.contain({path: './test.log'});
+    expect(l).to.contain({path: './test/fixtures/short-test.log'});
   });
 
   it('should have a data property', async () => {
-    const l = await Log.logfile('./test.log');
+    const l = await Log.logfile('./test/fixtures/short-test.log');
 
     expect(l.data).to.exist();
   });
 
-  it('should read the file in path and store the data in the data property', async () => {
-    const l = await Log.logfile('./test.log');
+  it('should read the file in path and store the data in the data property if line length is 3 or less', async () => {
+    const l = await Log.logfile('./test/fixtures/short-test.log');
 
-    expect(l.data).to.satisfy(() => {
-      return (l.data === '1) test-test-test\n2) test-test-test\n3) test-test-test\n4) test-test-test\n' || l.data === '1) test-test-test\r\n2) test-test-test\r\n3) test-test-test\r\n4) test-test-test\r\n' ? true : false);
-    });
+    if (l.length <= 3) {
+      expect(l.data).to.satisfy(() => {
+        return (l.data === '1) test-test-test\n2) test-test-test\n3) test-test-test\n' || l.data === '1) test-test-test\r\n2) test-test-test\r\n3) test-test-test\r\n' ? true : false);
+      });
+    }
   });
 
   it('should count log line length accurately', async () => {
-    const l = await Log.logfile('./test.log');
+    const l = await Log.logfile('./test/fixtures/short-test.log');
 
-    expect(l.length).to.equal(4);
+    expect(l.length).to.equal(3);
   });
 });
